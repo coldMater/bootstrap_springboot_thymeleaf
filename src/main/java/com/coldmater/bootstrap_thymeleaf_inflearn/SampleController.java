3bf6ed9fc983364e5769977de66cd2e82b073a01
coldMater <coldmater@gmail.com>
2020-08-03 23:20:18 +0900

    [modal] modal, modal-dialog, modal-content, modal-#[header, body, footer], modal-title

--- src/main/java/com/coldmater/bootstrap_thymeleaf_inflearn/SampleController.java
+++ src/main/java/com/coldmater/bootstrap_thymeleaf_inflearn/SampleController.java
@@ -128,4 +128,9 @@ public class SampleController {
     public String carousel() {
         return "carousel";
     }
+
+    @GetMapping("/modal")
+    public String modal() {
+        return "modal";
+    }
 }
