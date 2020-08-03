896f0d0ea9de1b4a60451fd87b91528506d9297a
coldMater <coldmater@gmail.com>
2020-08-04 00:14:27 +0900

    [utility-class1]

--- src/main/java/com/coldmater/bootstrap_thymeleaf_inflearn/SampleController.java
+++ src/main/java/com/coldmater/bootstrap_thymeleaf_inflearn/SampleController.java
@@ -7,30 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SampleController {
 
     @GetMapping("/hello")
-    public String hello() {
-
-        return "hello";
-    }
+    public String hello() { return "hello"; }
 
     @GetMapping("/grid")
-    public String grid() {
-        return "grid";
-    }
+    public String grid() { return "grid"; }
 
     @GetMapping("/typography")
-    public String typography() {
-        return "typography";
-    }
+    public String typography() { return "typography"; }
 
     @GetMapping("/typo-css")
-    public String typoCss() {
-        return "typo-css";
-    }
+    public String typoCss() { return "typo-css"; }
 
     @GetMapping("/color")
-    public String color() {
-        return "color";
-    }
+    public String color() { return "color"; }
 
     @GetMapping("/table-responsive")
     public String tableResponsive() {
@@ -38,105 +27,68 @@ public class SampleController {
     }
 
     @GetMapping("/table")
-    public String table() {
-        return "table";
-    }
+    public String table() { return "table"; }
 
     @GetMapping("/image")
-    public String image() {
-        return "image";
-    }
+    public String image() { return "image"; }
 
     @GetMapping("/jumbotron")
-    public String jumbotron() {
-        return "jumbotron";
-    }
+    public String jumbotron() { return "jumbotron"; }
 
     @GetMapping("/alert")
-    public String alert() {
-        return "alert";
-    }
+    public String alert() { return "alert"; }
 
     @GetMapping("/button")
-    public String button() {
-        return "button";
-    }
+    public String button() { return "button"; }
 
     @GetMapping("/badge")
-    public String badge() {
-        return "badge";
-    }
+    public String badge() { return "badge"; }
 
     @GetMapping("/progress")
-    public String progress() {
-        return "progress";
-    }
-
+    public String progress() { return "progress"; }
 
     @GetMapping("/pagination")
-    public String pagination() {
-        return "pagination";
-    }
+    public String pagination() { return "pagination"; }
 
     @GetMapping("/breadcrumb")
-    public String breadcrumb() {
-        return "breadcrumb";
-    }
+    public String breadcrumb() { return "breadcrumb"; }
 
     @GetMapping("/listgroup")
-    public String listgroup() {
-        return "listgroup";
-    }
-
+    public String listgroup() { return "listgroup"; }
 
     @GetMapping("/card")
-    public String card() {
-        return "card";
-    }
+    public String card() { return "card"; }
 
     @GetMapping("/form")
-    public String form() {
-        return "form";
-    }
+    public String form() { return "form"; }
 
     @GetMapping("/input-group")
-    public String inputGroup() {
-        return "input-group";
-    }
+    public String inputGroup() { return "input-group"; }
 
     @GetMapping("/input")
-    public String input() {
-        return "input";
-    }
+    public String input() { return "input"; }
 
     @GetMapping("/dropdown")
-    public String dropdown() {
-        return "dropdown";
-    }
+    public String dropdown() { return "dropdown"; }
 
     @GetMapping("/tabpills")
-    public String tablpills() {
-        return "tabpills";
-    }
+    public String tablpills() { return "tabpills"; }
 
     @GetMapping("/navbar")
-    public String navBar() {
-        return "navBar";
-    }
+    public String navBar() { return "navBar"; }
 
     @GetMapping("/carousel")
-    public String carousel() {
-        return "carousel";
-    }
+    public String carousel() { return "carousel"; }
 
     @GetMapping("/modal")
-    public String modal() {
-        return "modal";
-    }
+    public String modal() { return "modal"; }
 
     @GetMapping("/tooltip")
     public String tooltip() { return "tooltip";}
 
     @GetMapping("/popover")
     public String popOver() { return "popover";}
+
+    @GetMapping("/utility-class")
+    public String utilityClass() { return "utility-class";}
 }
